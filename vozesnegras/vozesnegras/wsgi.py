8a0bbd71e6e2f634e9b2a5d8db0da19153c27f9b
Lipe <lipe099@gmail.com>
2025-11-27 14:51:41 -0300

    correções

--- vozesnegras/vozesnegras/wsgi.py
+++ vozesnegras/vozesnegras/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Aplicativo project.
+WSGI config for vozesnegras project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Aplicativo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vozesnegras.settings')
 
 application = get_wsgi_application()
