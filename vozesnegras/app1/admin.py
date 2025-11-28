0c9f721af1ff4fcf55df0dbbf97bbc30256abca0
Lipe <lipe099@gmail.com>
2025-11-28 15:01:13 -0300

    versão final

--- vozesnegras/app1/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Postagem, Categoria, Perfil, Voto
-
-# Isso faz as tabelas aparecerem no painel /admin
-admin.site.register(Postagem)
-admin.site.register(Categoria)
-admin.site.register(Perfil)
-admin.site.register(Voto)
